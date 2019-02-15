d810b4eb603199974f2acfb7925b62b52f0a8331
Alexander Kraus <nr4@z10.info>
2019-02-15 14:34:25 +0100

    added random color provider class

---
+++ tools/ColorProvider.cpp
@@ -0,0 +1,32 @@
+/* SAEGE - ShAdEr Graph Editor v1.57 "BINGE" 
+ * Copyright (C) 2019  Alexander Kraus <nr4@z10.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "ColorProvider.hpp"
+#include <QRandomGenerator>
+
+ColorProvider::ColorProvider()
+{
+}
+
+ColorProvider::~ColorProvider()
+{
+}
+
+int ColorProvider::provideDark()
+{
+    return QColor::fromHSV(.05+.9*QRandomGenerator::global()->generate(), .8, .88);
+}
