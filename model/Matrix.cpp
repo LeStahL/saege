a03653b09b739b77c7feb2bb85b7f1d09bf109fe
Alexander Kraus <nr4@z10.info>
2019-03-03 14:26:03 +0100

    added matrix model

---
+++ model/Matrix.cpp
@@ -0,0 +1,174 @@
+/* SAEGE - ShAdEr Graph Editor v1.57 "BINGE" 
+ * Copyright (C) 2019  Alexander Kraus <nr4@z10.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "Matrix.hpp"
+
+#include <QtGlobal>
+
+void * Matrix::data(int row, int column)
+{
+    if(m_data.size() <= row || row < 0) return false;
+    if(m_cols <= column || column < 0) return false;
+
+    return m_data[row][column];
+}
+
+bool Matrix::addCoulumns(int ncolumns)
+{
+    if(ncolumns < 0) return false;
+    
+    m_cols += ncolumns;
+    
+    for(int i=0; i<m_data.size(); ++i)
+    {
+        for(int j=0; j<ncolumns; ++j)
+        {
+            m_on[i].push_back(0);
+            m_data[i].push_back(0);
+        }
+    }
+    
+    return true;
+}
+
+bool Matrix::removeColumns(int ncolumns)
+{
+    if(ncolumns < 0) return false;
+    
+    m_cols -= qMax(ncolumns, m_cols);
+    for(int i=0; i<m_data.size(); ++i)
+    {
+        for(int j=0; j<qMax(ncolumns, m_cols); ++j)
+        {
+            m_on[i].pop_back();
+            m_data[i].pop_back();
+        }
+    }
+    return true;
+}
+
+bool Matrix::addRow(QString name)
+{
+    m_row_names.push_back(name);
+    
+    QList<void *> lv;
+    QList<int> li;
+    for(int i=0; i<m_cols; ++i)
+    {
+        lv.push_back(0);
+        li.push_back(0);
+    }
+    m_data.push_back(lv);
+    m_on.push_back(li);
+    
+    return true;
+}
+
+bool Matrix::removeRow(QString name)
+{
+    if(!m_row_names.contains(name)) return false;
+    
+    int index = m_row_names.indexOf(name);
+    m_row_names.removeAt(index);
+    m_data.removeAt(index);
+    m_on.removeAt(index);
+    
+    return true;
+}
+
+Matrix::Matrix(QString name)
+    : m_cols(24)
+    , m_name("")
+    , m_time_step(QTime(0,0,1,0))
+{
+}
+
+Matrix::~Matrix()
+{
+}
+
+void Matrix::setTimeStep(QTime step)
+{
+    m_time_step = step;
+}
+
+bool Matrix::setData(int row, int column, void* data)
+{
+    if(m_data.size() <= row || row < 0) return false;
+    if(m_cols <= column || column < 0) return false;
+    
+    m_data[row][column] = data;
+    return true;
+}
+
+bool Matrix::setValue(int row, int column, int value)
+{
+    if(m_data.size() <= row || row < 0) return false;
+    if(m_cols <= column || column < 0) return false;
+    
+    m_on[row][column] = value;
+    return true;
+}
+
+int Matrix::value(int row, int column)
+{
+    if(m_data.size() <= row || row < 0) return false;
+    if(m_cols <= column || column < 0) return false;
+    
+    return m_on[row][column];
+}
+
+QTime Matrix::time(int column)
+{
+    int step_msecs = m_time_step.msecsSinceStartOfDay();
+    return QTime(0,0,0,step_msecs*column);
+}
+
+QTime Matrix::timeStep()
+{
+    return m_time_step;
+}
+
+QString Matrix::rowName(int row)
+{
+    if(row < 0 || m_row_names.size() <= row) return "Error";
+    return m_row_names[row];
+}
+
+QString Matrix::toString()
+{
+    QString ret;
+    for(int i=0; i<m_on.size(); ++i)
+    {
+        for(int j=0; j<m_cols; ++j)
+            ret += QString::number(m_on[i][j])+ " ";
+        ret += "\n";
+    }
+    return ret;
+}
+
+void Matrix::setName(QString name)
+{
+    m_name = name;
+}
+
+QString Matrix::name()
+{
+    return m_name;
+}
+
+
---
+++ model/Matrix.hpp
@@ -0,0 +1,65 @@
+/* SAEGE - ShAdEr Graph Editor v1.57 "BINGE" 
+ * Copyright (C) 2019  Alexander Kraus <nr4@z10.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <QString>
+#include <QList>
+#include <QVariant>
+#include <QTime>
+
+class Matrix 
+{
+public:
+    Matrix(QString name);
+    virtual ~Matrix();
+    
+    //TODO: add toxml, fromxml for serialization
+//     QString toXml();
+//     void fromXml(QString source);
+    
+    QString toString();
+    
+    bool addRow(QString name);
+    bool removeRow(QString name);
+    
+    bool addCoulumns(int ncolumns);
+    bool removeColumns(int ncolumns);
+    
+    QString rowName(int row);
+    void *data(int row, int column);
+    int value(int row, int column);
+    bool setData(int row, int column, void *data);
+    bool setValue(int row, int column, int value);
+    QTime time(int column);
+    QTime timeStep();
+    void setTimeStep(QTime step);
+    
+    QString name();
+    void setName(QString name);
+    
+private:
+    QTime m_time_step;
+    QList<QString> m_row_names;
+    QList<QList<int> > m_on;
+    QList<QList<void *> > m_data;
+    QString m_name;
+    int m_cols;
+};
+
+#endif
